35044744ffc66d655e6c61b96abee95fd70a87aa
batman-4 <22100409@handong.ac.kr>
2023-09-03 21:06:54 +0900

    added menu 0,1,4

---
+++ src/main/java/org/example/ICRUD.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public interface ICRUD {
+    //add functions 4
+    public Object add();
+    public int update(Object obj);
+    public int delete(Object obj);
+    public void selectOne (int id);
+}
---
+++ src/main/java/org/example/WordManager.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class WordManager {
+    //create selectMenu function
+    Scanner s = new Scanner(System.in);
+    WordCRUD wordCRUD;
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
+
+    public int selectMenu() {
+        System.out.println("*** 영단어 마스터 ***\n" +
+                "********************\n" +
+                "1. 모든 단어 보기\n" +
+                "2. 수준별 단어 보기\n" +
+                "3. 단어 검색\n" +
+                "4. 단어 추가\n" +
+                "5. 단어 수정\n" +
+                "6. 단어 삭제\n" +
+                "7. 파일 저장\n" +
+                "0. 나가기\n" +
+                "********************");
+        System.out.println("원하는 메뉴는?");
+        return s.nextInt();
+    }
+
+    public void start() {
+        while(true) {
+            int choice = selectMenu();
+            if(choice == 0) {
+                System.out.println("Program will be ended.");
+                break;
+            }
+            if(choice == 1) {
+                wordCRUD.listAll();
+            }
+            else if (choice == 4) {
+                wordCRUD.addWord();
+            }
+        }
+    }
+}
